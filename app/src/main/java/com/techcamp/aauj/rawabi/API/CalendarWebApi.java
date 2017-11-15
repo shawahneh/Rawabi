013fc1cf16737955e5551d4bfaee65b414aaf371
alaamarneh <ala.amarneh@hotmail.com>
2017-11-15 16:44:17 +0200

    added string

---
+++ app/src/main/java/com/techcamp/aauj/rawabi/API/CalendarWebApi.java
@@ -0,0 +1,14 @@
+package com.techcamp.aauj.rawabi.API;
+
+import com.techcamp.aauj.rawabi.Beans.Event;
+import com.techcamp.aauj.rawabi.ITriger;
+
+import java.util.Date;
+
+/**
+ * Created by alaam on 11/15/2017.
+ */
+
+public interface CalendarWebApi {
+    void getEventAtDate(Date date, ITriger<Event> eventITriger);
+}
---
+++ app/src/main/java/com/techcamp/aauj/rawabi/Beans/Event.java
@@ -0,0 +1,45 @@
+package com.techcamp.aauj.rawabi.Beans;
+
+import java.util.Date;
+
+/**
+ * Created by alaam on 11/15/2017.
+ */
+
+public class Event {
+    private String name,description,imageUrl;
+    private Date date;
+    public Event(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}

a652a869ac961fe93f59a312743d06bb63583199
Mohammad Shawahneh <mohammad.s.shawahneh@gmail.com>
2017-11-15 13:16:10 +0200

    Webservice

--- app/src/main/java/com/techcamp/aauj/rawabi/API/WebService.java
+++ app/src/main/java/com/techcamp/aauj/rawabi/API/WebService.java
@@ -7,6 +7,7 @@ package com.techcamp.aauj.rawabi.API;
 public class WebService {
     public String name;
     public WebService(){
-
+        name = "mooooggg";
+        name = name + ":";
     }
 }

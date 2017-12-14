fe5dc58c6c8c8a5c6f55d382ded72f415a497768
Mohammad Shawahneh <mohammad.s.shawahneh@gmail.com>
2017-12-15 01:19:10 +0200

    Webservicen

--- app/src/main/java/com/techcamp/aauj/rawabi/Beans/Ride.java
+++ app/src/main/java/com/techcamp/aauj/rawabi/Beans/Ride.java
@@ -1,5 +1,7 @@
 package com.techcamp.aauj.rawabi.Beans;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by User on 11/16/2017.
  */
@@ -8,20 +10,24 @@ public class Ride {
     private int id;
     private User user;
     private Journey journey;
-    private int meetingLocationX;
-    private int meetingLocationY;
-    private int orderStatus;//Pending : 0 , Accepted : 1 , rejected : 2, cancelled from rider : 4
+    private LatLng meetingLocation;
+    private int orderStatus;//Pending : 0 , Accepted : 1 , rejected : 2
 
     public Ride(){
 
     }
-    public Ride(int id, User user, Journey journey, int meetingLocationX, int meetingLocationY, int orderStatus) {
+
+
+    public Ride(int id, User user, Journey journey, LatLng meetingLocation, int orderStatus) {
         this.id = id;
         this.user = user;
         this.journey = journey;
-        this.meetingLocationX = meetingLocationX;
-        this.meetingLocationY = meetingLocationY;
+        this.meetingLocation = meetingLocation;
         this.orderStatus = orderStatus;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getId() {
@@ -34,30 +40,31 @@ public class Ride {
         return user;
     }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
 
 
     public Journey getJourney() {
         return journey;
     }
 
-    public int getMeetingLocationX() {
-        return meetingLocationX;
-    }
-
-    public void setMeetingLocationX(int meetingLocationX) {
-        this.meetingLocationX = meetingLocationX;
-    }
-
-    public int getMeetingLocationY() {
-        return meetingLocationY;
-    }
-
-    public void setMeetingLocationY(int meetingLocationY) {
-        this.meetingLocationY = meetingLocationY;
+    public void setJourney(Journey journey) {
+        this.journey = journey;
     }
 
     public int getOrderStatus() {
         return orderStatus;
+    }
+
+
+
+    public LatLng getMeetingLocation() {
+        return meetingLocation;
+    }
+
+    public void setMeetingLocation(LatLng meetingLocation) {
+        this.meetingLocation = meetingLocation;
     }
 
     public void setOrderStatus(int orderStatus) {
